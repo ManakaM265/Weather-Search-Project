0efbf16cf882ee5df6bd5fd9127fbb5d6f44ce90
Mahadi <manakamedia@gmail.com>
2025-04-16 23:43:19 +0200

    Injected html forecast from Javascript
    1. Removed weather forecast html
    2. Added id "forecast" to the weather forecast
    3. Targeted id "forecast" from JavaScript to insert html via a loop

--- src/index.js
+++ src/index.js
@@ -55,4 +55,30 @@ function handleSearchSubmit(event) {
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
+function displayForecast() {
+  let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+      <div class="weather-forecast-day">
+        <div class="weather-forecast-date">${day}</div>
+        <div class="weather-forecast-icon">🌤️</div>
+        <div class="weather-forecast-temperatures">
+          <div class="weather-forecast-temperature">
+            <strong>15º</strong>
+          </div>
+          <div class="weather-forecast-temperature">9º</div>
+        </div>
+      </div>
+    `;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
 searchCity("Johannesburg");
+displayForecast();
